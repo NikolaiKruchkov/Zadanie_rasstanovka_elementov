835a4e5f6b63101218e8a06e31df0ccffb628e56
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-02-17 14:43:26 +0300

    Add files via upload

---
+++ RasstanovkaObj/ColumnUtils.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasstanovkaObj
+{
+    class ColumnUtils
+    {
+        public static List<FamilySymbol> GetColumnTypes(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var familySymbols = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_Columns)
+                    .OfClass(typeof(FamilySymbol))
+                    .Cast<FamilySymbol>()
+                    .ToList();
+            return familySymbols;
+        }
+    }
+}
---
+++ RasstanovkaObj/MainViewViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasstanovkaObj
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+        public FamilySymbol SelectedColumnType { get; set; }
+        public Level SelectedLevel { get; set; }
+        public List<FamilySymbol> TypeColumns { get; } = new List<FamilySymbol>();
+        public List<Level> Levels { get; } = new List<Level>();
+        public DelegateCommand SaveCommand { get; }
+        public List<XYZ> Points { get; } = new List<XYZ>();
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            TypeColumns = ColumnUtils.GetColumnTypes(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            Points = SelectionUtils.GetPoints(commandData, "Выберите точки", Autodesk.Revit.UI.Selection.ObjectSnapTypes.Endpoints);
+        }
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (SelectedColumnType == null || SelectedLevel == null)
+                return;
+            foreach (XYZ point in Points)
+            {
+                FamilyInstance column = FamilyInstanceUtils.CreateFamilyInstance(_commandData, SelectedColumnType, point, SelectedLevel);
+            }
+            RaiseCloseRequest();
+        }
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
